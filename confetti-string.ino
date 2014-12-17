9a157ea7a0f9ede4f10f77bd2d35668b7ab48ce8
brencerddwr <rgbailey@gmail.com>
2014-12-16 21:38:56 -0800

    add comments to the file to explain what is going on in the script

--- confetti-string.ino
+++ confetti-string.ino
@@ -7,30 +7,41 @@
 //
 //////////////////////////////////////////////////
 
+// Change this to reflect the number of LEDS in your string
 #define NUM_LEDS 29
 
 // Data pin that led data will be written out over
 #define DATA_PIN 11
+
 // Clock pin only needed for SPI based chipsets when not using hardware SPI
 //#define CLOCK_PIN 8
 
+// Create array for the leds
 CRGB leds[NUM_LEDS];
 
 void setup() {
-	// sanity check delay - allows reprogramming if accidently blowing power w/leds
+	// sanity check delay - allows reprogramming if accidentally blowing power w/leds
 	delay(2000);
 
-
+	// setup LED strip with the array
 	FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
+	
+	// set initial master brightness of LEDS
 	FastLED.setBrightness(255);
 
 }
 
 void loop() {
+	// fade to black
 	fadeToBlackBy( leds, NUM_LEDS, 10);
+	
+	// pick a random led from the string
 	int x = random16(NUM_LEDS);
+	
+	// check if the led is off
 	if (!leds[x])
 	{
+		// if the led was off, set a random hue at full saturation and value
 		leds[ x ] += CHSV( random8(), 255, 255);
 	}
 	FastLED.show();
